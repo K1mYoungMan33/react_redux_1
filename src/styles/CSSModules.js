09313a8366aa67ecf8ab84e23af80bc93c4be8bb
김영맨33 <jangyhun@naver.com>
2024-03-21 15:52:46 +0900

    CSSModule.module.css  중첩되는 css 이름을 사용하는 경우

--- src/styles/CSSModules.js
+++ src/styles/CSSModules.js
@@ -1,8 +1,21 @@
 import styles from "./CSSModule.module.css";
+import styles2 from "./CSSModule2.module.css";
 
 
 export const CSSModules=()=>{
-    return <div className={styles.wrapper}>
-        안녕하세요, 저는 <span className="something">CSS Module!</span>
-    </div>
+
+    console.log( styles )
+    console.log( styles2 )
+
+    const MySpan = (()=><>안녕하세요, 저는 <span className="something">CSS Module!</span></>);
+
+    return <>
+            <div className={styles.wrapper}>
+                <MySpan/>
+            </div>
+            <div className={`${styles2.wrapper2} ${styles.inverted}`}>
+                <MySpan/>
+            </div>
+
+            </>
 }
