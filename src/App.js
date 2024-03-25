db4c0352d52b60da202fecade53b431dc12c144c
김영맨33 <jangyhun@naver.com>
2024-03-25 14:29:46 +0900

    NewsList 샘플데이터

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 import {useState} from "react";
 import axios from "axios";
+import {NewsList} from "./NewsList";
 
 export const  App=()=> {
+  const [ message, setMessage ] = useState( '1' );
   const [ data, setData ] = useState( null );
-  const onClick=()=>{
-    axios.get('https://jsonplaceholder.typicode.com/todos/1')
-        .then( response=> setData( response.data ) )
+  const onClick= async()=>{
+    try {
+      // window.gv = (window.gv ? window.gv : []).concat( {message} )
+      window.gv = [...( window.gv || [] ) ].concat( {message} )
+
+
+
+      // const response = await axios.get('https://jsonplaceholder.typicode.com/todos/' + {message}.message );
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/todos/${message}` );
+      setData(response.data);
+    }catch (e) {
+      console.log( e )
+    }
   }
     return (
       <div>
+        <input value={message} onChange={e=>setMessage(e.target.value)} />
         <div>
           <button onClick={onClick}>불러오기</button>
         </div>
@@ -21,6 +34,8 @@ export const  App=()=> {
         , 2 )} readOnly={true} /> }
           <hr/>
         {data && <textarea rows={7} value={JSON.stringify(data, [ "id", "title" ], 2 )} readOnly={true} /> }
+
+        <NewsList />
     </div>
   );
 };
---
+++ src/NewsList.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import {NewsItem} from "./NewsItem";
+
+const NewsListBlock = styled.div`
+    box-sizing: border-box;
+    padding-bottom: 3rem;
+    width: 768px;
+    margin: 0 auto;
+    margin-top: 2rem;
+    @media screen and (max-width: 768px) {
+        width: 100%;
+        padding-left: 1rem;
+        padding-right: 1rem;
+    }
+`;
+
+const sampleArticle = {
+    title: '제목',
+    description: '내용',
+    url: 'https://google.com',
+    urlToImage: 'https://via.placeholder.com/160',
+};
+
+export const NewsList=()=>{
+    return (
+        <NewsListBlock>
+            {[ ..."111116" ].map(()=>
+            <NewsItem article={sampleArticle} />
+                )}
+        </NewsListBlock>
+    )
+}
