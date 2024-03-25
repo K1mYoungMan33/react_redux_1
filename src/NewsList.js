57d888cf0757594709530b11bac292da188049d8
김영맨33 <jangyhun@naver.com>
2024-03-25 15:57:28 +0900

    NewsList 커스텀 Hook usePromise 생성

--- src/NewsList.js
+++ src/NewsList.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import {NewsItem} from "./NewsItem";
 import {useEffect, useState} from "react";
 import axios from "axios";
+import usePromise from "./lib/usePromise";
 
 const NewsListBlock = styled.div`
     box-sizing: border-box;
@@ -24,39 +25,67 @@ const sampleArticle = {
 };
 
 export const NewsList=( { category } )=>{
-    const [ articles, setArticles ] = useState( null );
-    const [ loading, setLoading ] = useState( false );
-
-    useEffect(() => {
-        const fetchData = async()=>{
-            setLoading( true );
-            try {
-                const query = category === 'all' ? '' : `&category=${category}`;
-                const response = await axios.get(
-                    `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=0a8c4202385d4ec1bb93b7e277b3c51f`,
-                );
-                setArticles( response.data.articles );
-            } catch( e ) {
-                console.log( e );
-            }
-            setLoading( false );
-        };
-        fetchData();
+    const [ loading, response, error ] = usePromise(()=>{
+        const query = category === 'all' ? '' : `&category=${category}`;
+        return axios.get(
+            `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=0a8c4202385d4ec1bb93b7e277b3c51f`,
+        );
     }, [category]);
 
     if ( loading ) {
         return <NewsListBlock>대기 중...</NewsListBlock>;
     }
 
-    if ( !articles ) {
+    if ( !response ) {
         return null;
     }
 
+    if ( error ) {
+        return <NewsListBlock>에러 발생!</NewsListBlock>;
+    }
+
+    const { articles } = response.data;
     return (
         <NewsListBlock>
-            {articles.map( article=>
-            <NewsItem key={article.url} article={article} />
-                )}
+            { articles.map( article =>
+                <NewsItem key={article.url} article={article} />
+            )}
         </NewsListBlock>
     )
+
+    // const [ articles, setArticles ] = useState( null );
+    // const [ loading, setLoading ] = useState( false );
+    //
+    // useEffect(() => {
+    //     const fetchData = async()=>{
+    //         setLoading( true );
+    //         try {
+    //             const query = category === 'all' ? '' : `&category=${category}`;
+    //             const response = await axios.get(
+    //                 `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=0a8c4202385d4ec1bb93b7e277b3c51f`,
+    //             );
+    //             setArticles( response.data.articles );
+    //         } catch( e ) {
+    //             console.log( e );
+    //         }
+    //         setLoading( false );
+    //     };
+    //     fetchData();
+    // }, [category]);
+    //
+    // if ( loading ) {
+    //     return <NewsListBlock>대기 중...</NewsListBlock>;
+    // }
+    //
+    // if ( !articles ) {
+    //     return null;
+    // }
+    //
+    // return (
+    //     <NewsListBlock>
+    //         {articles.map( article=>
+    //         <NewsItem key={article.url} article={article} />
+    //             )}
+    //     </NewsListBlock>
+    // )
 }
