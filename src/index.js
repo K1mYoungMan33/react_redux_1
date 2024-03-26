f716aff541ab8ca45023b1079cb9694d398f8a35
김영맨33 <jangyhun@naver.com>
2024-03-26 11:51:18 +0900

    모듈(이벤트 정의값, 이벤트 처리 함수, 초기값, 리듀스) 및 todos 작성

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import {App} from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
+import {rootReducer} from "./modules";
+import { Provider } from "react-redux";
+// import { composeWithDevTools } from 'redux-devtools-extension';
+
+// const store = createStore( rootReducer );
+const store = createStore( rootReducer, /* preloadedState, */ window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() );
+// const store = createStore( rootReducer, composeWithDevTools());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <Provider store={store}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
