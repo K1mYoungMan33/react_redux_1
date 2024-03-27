164533579d87a5b4991ebd121b74be097b162749
김영맨33 <jangyhun@naver.com>
2024-03-27 15:43:29 +0900

    axios 사용하여 post users 받아오는 것 기본 형태 작성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import CounterContainer from "./components/CounterContainer";
+import SampleContainer from "./components/SampleContainer";
 
 export const App=()=> <div>
   <CounterContainer />
+  <SampleContainer />
 </div>;
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,6 +1,8 @@
 import {combineReducers} from "redux";
 import {counter} from "./counter";
+import {sample} from "./sample";
 
 export const rootReducer = combineReducers({
     counter,
+    sample,
 });
