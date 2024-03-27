951721b943f3bd0b83628f6e181c0bcaaccc01b3
김영맨33 <jangyhun@naver.com>
2024-03-27 12:25:19 +0900

    useSelector useDispatch 적용

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,12 +1,19 @@
 import {Counter} from "../components/Counter";
-import {connect} from "react-redux";
+import {connect, useDispatch, useSelector} from "react-redux";
 import {decrease, increase} from "../modules/counter";
 
-// export const CounterContainer=()=> <Counter />;
-
-const CounterContainer=( { number, increase, decrease } ) =>
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />;
-
+// const CounterContainer=( { number, increase, decrease } ) =>
+//     <Counter number={number} onIncrease={increase} onDecrease={decrease} />;
+const CounterContainer=()=>{
+    const number = useSelector( state => state.counter.number );
+    const dispatch = useDispatch();
+    return <Counter number={number}
+                    onIncrease={()=>dispatch(increase())}
+                    onDecrease={()=>dispatch(decrease())}
+    />
+}
+export default CounterContainer;
+/*
 const mapStateToProps = state => ({
     number: state.counter.number,
 });
@@ -22,5 +29,6 @@ const mapDispatchToProps = dispatch=> ({
 
 export default connect(
     mapStateToProps,
-    mapDispatchToProps, // 두번째 인자는 bindActionCreators 
+    mapDispatchToProps, // 두번째 인자는 bindActionCreators
 )(CounterContainer);
+*/
--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,27 +1,50 @@
 import {Todos} from "../components/Todos";
-import {connect} from "react-redux";
+import {connect, useDispatch, useSelector} from "react-redux";
 import {changeInput, insert, remove, toggle} from "../modules/todos";
+import {useCallback} from "react";
 
-const TodosContainer=({
-    input,
-    todos,
-    changeInput,
-    insert,
-    toggle,
-    remove,
-}) => {
+const TodosContainer=(
+// { input, todos, changeInput, insert, toggle, remove, }
+) => {
+    const { input, todos } = useSelector( ({todos}) => ({
+        input : todos.input,
+        todos: todos.todos,
+    }));
+    const dispatch = useDispatch();
+    const onChangeInput = useCallback( input=>
+        dispatch( changeInput(input))
+        , [ dispatch ]
+    );
+    const onInsert = useCallback( text =>
+        dispatch( insert(text))
+        , [ dispatch]
+    );
+    const onToggle = useCallback( id =>
+        dispatch( toggle( id ))
+        , [ dispatch ]
+    );
+    const onRemove = useCallback( id =>
+        dispatch( remove( id ) )
+        , [ dispatch ]
+    );
+
     return (
         <Todos
             input={input}
             todos={todos}
-            onChangeInput={changeInput}
-            onInsert={insert}
-            onToggle={toggle}
-            onRemove={remove}
+            // onChangeInput={changeInput}
+            // onInsert={insert}
+            // onToggle={toggle}
+            // onRemove={remove}
+            onChangeInput={onChangeInput}
+            onInsert={onInsert}
+            onToggle={onToggle}
+            onRemove={onRemove}
             />
     );
 };
-
+export default TodosContainer;
+/*
 export default connect(
     // 비구조화 할당을 통해 todos 를 분리하여
     // state.todos.input 대신 todos.input 을 사용
@@ -36,3 +59,4 @@ export default connect(
         remove,
     },
 )(TodosContainer);
+*/
