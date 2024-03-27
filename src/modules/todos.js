991bbf370d9dff9188050bd473f4855f3307e808
김영맨33 <jangyhun@naver.com>
2024-03-27 11:33:34 +0900

    immer draft 적용

--- src/modules/todos.js
+++ src/modules/todos.js
@@ -1,4 +1,5 @@
 import {createAction, handleActions} from "redux-actions";
+import {produce} from "immer";
 
 const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // 인풋 값을 변경함
 const INSERT = 'todos/INSERT'; // 새로운 todo를 등록함
@@ -50,33 +51,43 @@ const initialState = {
 export const todos = handleActions(
     // switch ( action.type ) {
     {
-        // case CHANGE_INPUT:
-        //     return {
-        //         ...state,
-        //         input: action.input
-        //     };
-        [CHANGE_INPUT]: (state, action) => ({ ...state, input: action.payload }),
-        // case INSERT:
-        //     return {
-        //         ...state,
-        //         todos: state.todos.concat( action.todo )
-        //     };
-        [INSERT]: (state, action) => ({ ...state, todos:state.todos.concat( action.payload )}),
-        // case TOGGLE:
-        //     return {
-        //         ...state,
-        //         todos: state.todos.map( todo =>
-        //             todo.id === action.id ? { ...todo, done: !todo.done } : todo
-        //         )
-        //     };
-        [TOGGLE]: (state, action) => ({ ...state, todos:state.todos.map( todo=>
-            todo.id === action.payload ? { ...todo, done: !todo.done } : todo )}),
-        // case REMOVE:
-        //     return {
-        //         ...state,
-        //         todos: state.todos.filter( todo => todo.id !== action.id )
-        //     };
-        [REMOVE]: (state, action) => ({ ...state, todos: state.todos.filter( todo => todo.id !== action.payload )}),
+        // // case CHANGE_INPUT:
+        // //     return {
+        // //         ...state,
+        // //         input: action.input
+        // //     };
+        // [CHANGE_INPUT]: (state, {payload:input} ) => ({ ...state, input: input }),
+        [CHANGE_INPUT]: (state, {payload:input} ) => produce( state, draft => { draft.input = input }),
+        // // case INSERT:
+        // //     return {
+        // //         ...state,
+        // //         todos: state.todos.concat( action.todo )
+        // //     };
+        // [INSERT]: (state, {payload:todo} ) => ({ ...state, todos:state.todos.concat( todo )}),
+        [INSERT]: (state, {payload:todo} ) => produce(state, draft=> { draft.todos.push( todo )}),
+        // // case TOGGLE:
+        // //     return {
+        // //         ...state,
+        // //         todos: state.todos.map( todo =>
+        // //             todo.id === action.id ? { ...todo, done: !todo.done } : todo
+        // //         )
+        // //     };
+        // [TOGGLE]: (state, {payload:id} ) => ({ ...state, todos:state.todos.map( todo=>
+        //     todo.id === id ? { ...todo, done: !todo.done } : todo )}),
+        [TOGGLE]: (state, {payload:id} ) => produce( state, draft=> {
+            const todo = draft.todos.find( inTodo => inTodo.id === id);
+            todo.done = !todo.done;
+        }),
+        // // case REMOVE:
+        // //     return {
+        // //         ...state,
+        // //         todos: state.todos.filter( todo => todo.id !== action.id )
+        // //     };
+        // [REMOVE]: (state, {payload:id }) => ({ ...state, todos: state.todos.filter( todo => todo.id !== id )}),
+        [REMOVE]: (state, {payload:id }) => produce( state, draft=> {
+            const index = draft.todos.findIndex(todo => todo.id !== id);
+            draft.todos.splice(index, 1);
+        }),
         // default:
         //     return  state;
     }, initialState )
